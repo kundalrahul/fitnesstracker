cdfdd5f7a9378ef9cace393ef28ace15f66670af
kundalrahul <kundalrahul11@gmail.com>
2024-06-24 14:47:26 -0400

    Test

--- src/test/java/com/example/fitnesstracker/controller/WorkoutControllerTest.java
+++ src/test/java/com/example/fitnesstracker/controller/WorkoutControllerTest.java
@@ -7,9 +7,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Arrays;
@@ -22,7 +24,8 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(WorkoutController.class)
+@ActiveProfiles("test")
+@WebMvcTest(controllers  = WorkoutController.class, excludeAutoConfiguration = {DataSourceAutoConfiguration.class})
 public class WorkoutControllerTest {
 
     @Autowired
